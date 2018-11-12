6a0224b13043acaf57106f34c0cd2c8e7385bbfc
s1720094 <33256755+s1720094@users.noreply.github.com>
2018-11-12 18:01:50 +0000

    Update MoneyTest.java

--- MoneyTest.java
+++ MoneyTest.java
@@ -4,6 +4,9 @@
 package auctionhouse;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
@@ -19,7 +22,6 @@ public class MoneyTest {
         Money result = val1.add(val2);
         assertEquals("13.00", result.toString());
     }
-    
 
     /*
      ***********************************************************************
@@ -27,7 +29,6 @@ public class MoneyTest {
      ***********************************************************************
      * Add all your JUnit tests for the Money class below.
      */
-    
     @Test
     public void testSubtract() {
         Money val1 = new Money("14.50");
@@ -47,7 +48,7 @@ public class MoneyTest {
     @Test
     public void testToString() {
         Money val = new Money("25.111111");
-        Money result = val.toString();
+        String result = val.toString();
         assertEquals("25.11",result);
     }
     
@@ -84,12 +85,10 @@ public class MoneyTest {
         Money val1 = new Money("7.50");
         Money val2 = new Money("1.25");
         Money val3 = new Money("7.50");
-        double val4 = 7.50;
         boolean equals = val1.equals(val3);
         boolean notequals1 = val1.equals(val2);
-        boolean notequals2 = val1.equals(val4);
         assertTrue(equals);
-        assertFalse(notequals1 && notequals1);
+        assertFalse(notequals1);
         
     }
     
@@ -97,9 +96,9 @@ public class MoneyTest {
     @Test
     public void testHashCode() {
         Money val = new Money("32.75");
-        Money result = val.hashCode();
-        assertEquals("3275", result.toString());
-    }
+        int result = val.hashCode();
+        assertEquals(3275, result);
+}
 
     /*
      * Put all class modifications above.
